919389b77efa047693b4802cf82d7a85b4b06ecc
Hasiojjosiah <ticoyjosiahl@gmail.com>
2020-08-14 14:23:51 +0800

    Auth Blade Updated

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+    <div class="container">
+        <a href="#" class="navbar-brand">
+            <img src="{{ asset('dist/img/LD.png') }}" alt="Logo" class="brand-image img-circle elevation-3"
+                style="opacity: .8">
+            <span class="brand-text font-weight-light">LaravelDemo</span>
+        </a>
+    </div>
+</nav>
